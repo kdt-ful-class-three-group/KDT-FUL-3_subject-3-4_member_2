c31495eae8344322c0543ce55aadc7ecc7a6f9c2
kimyohun <yohunkim0@gmail.com>
2025-02-20 11:36:14 +0900

    index.html 첫 페이지 기동

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log("초기화 branch 확인작업");
+const http = require('http');
+const fs = require('fs');
+
+function firstPageSet(response){
+  const filePath = fs.readFileSync('./index.html')
+  response.writeHead(200, {'Content-Type' : 'text/html'}).end(filePath);
+}
+
+// * 서버 구동 부분 
+const server = http.createServer(function(request, response){
+  // * GET 방식 구동 
+  if(request.method === 'GET'){
+    // * 메인 페이지 표출하기
+    if(request.url === '/'){
+      firstPageSet(response);
+    }
+  }
+
+
+})
+
+// * 서버 시작 8000 포트 사용
+server.listen(8000, function(){
+  console.log("http://localhost:8000 서버 진행중")
+})
+
